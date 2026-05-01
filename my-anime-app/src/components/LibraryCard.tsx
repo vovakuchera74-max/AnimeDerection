baad43f0060edb1f8cc597b2e82c75adb3944a9d
Vova <vovakuchera74@gmail.com>
2026-05-01 21:17:33 +0300

    feat: implement supabase for favorites and watchlist

--- my-anime-app/src/components/LibraryCard.tsx
+++ my-anime-app/src/components/LibraryCard.tsx
@@ -1,31 +1,32 @@
-
 import { Star } from 'lucide-react';
-import type { Anime } from '../types/anime.types';
-import s from "../styles/LivraryCard.module.scss";
+import type { BaseAnime } from '../types/anime.types';
+import s from '../styles/LivraryCard.module.scss';
 import { useNavigate } from 'react-router-dom';
 
 interface Props {
-  anime: Anime;
+    anime: BaseAnime; 
 }
 
 export const LibraryCard = ({ anime }: Props) => {
     const navigate = useNavigate();
-  return (
-    <div className={s.card} onClick={() => navigate(`/anime/${anime.mal_id}`)}>
-        
-      <img
-        className={s.image}
-        src={anime.images.jpg.image_url}
-        alt={anime.title}
-      />
-      <div className={s.info}>
-        <p className={s.title}>{anime.title}</p>
-        <p className={s.episodes}>{anime.episodes} еп.</p>
-      </div>
-      <div className={s.rating}>
-        <Star size={22} color="#4a90e2" fill="#4a90e2" />
-        <span className={s.score}>{anime.score}</span>
-      </div>
-    </div>
-  );
+
+    const imageUrl = anime.image?.jpg?.image_url;
+
+    return (
+        <div className={s.card} onClick={() => navigate(`/anime/${anime.mal_id}`)}>
+            <img 
+                className={s.image}
+                src={imageUrl} 
+                alt={anime.title} 
+            />
+            <div className={s.info}>
+                <p className={s.title}>{anime.title}</p>
+                <p className={s.episodes}>{anime.episodes} ep.</p>
+            </div>
+            <div className={s.rating}>
+                <Star size={22} color="#4a90e2" fill="#4a90e2" />
+                <span className={s.score}>{anime.score}</span>
+            </div>
+        </div>
+    );
 };
